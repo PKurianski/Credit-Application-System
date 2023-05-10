3efc161634058f934667edd65dac4317af6f8d1b
PKurianski <pedrokurianski@gmail.com>
2023-05-10 15:24:09 -0300

    what did i do wrong

--- src/main/kotlin/com/example/demo/controller/CustomerResource.kt
+++ src/main/kotlin/com/example/demo/controller/CustomerResource.kt
@@ -1,4 +1,17 @@
 package com.example.demo.controller
 
-class CustomerResource {
+import com.example.demo.dto.CustomerDto
+import com.example.demo.service.impl.CustomerService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(private val customerService: CustomerService) {
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
+
+    }
 }
--- src/main/kotlin/com/example/demo/dto/CustomerDto.kt
+++ src/main/kotlin/com/example/demo/dto/CustomerDto.kt
@@ -1,2 +1,14 @@
-package com.example.demo.dto 
+package com.example.demo.dto
 
+import java.math.BigDecimal
+
+data class CustomerDto (
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String
+)
