b31a885000f2a234f438a90f099a90cc95febcec
PKurianski <pedrokurianski@gmail.com>
2023-05-11 18:42:52 -0300

    added min and max to number of installments

--- src/main/kotlin/com/example/demo/dto/CreditDto.kt
+++ src/main/kotlin/com/example/demo/dto/CreditDto.kt
@@ -5,12 +5,15 @@ import java.time.LocalDate
 import com.example.demo.entity.Credit
 import com.example.demo.entity.Customer
 import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 
 data class CreditDto(
     @field:NotNull(message = "Invalid Input, null") val creditValue: BigDecimal,
     @field:Future(message = "Date must be a present or future date") val dayFirstInstallment: LocalDate,
-    @field:NotNull(message = "Invalid Input, null") val numberOfInstallments: Int,
+    @field:NotNull(message = "Invalid Input, null")
+    @field:Min(1) @field:Max(48) val numberOfInstallments: Int,
     @field:NotNull(message = "Invalid Input, null") val customerId: Long
 ) {
 
