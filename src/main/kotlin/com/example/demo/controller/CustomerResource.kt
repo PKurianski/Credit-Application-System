0af7613ad5953bf55d45d8aef2b921068211dd34
PKurianski <pedrokurianski@gmail.com>
2023-05-21 14:42:04 -0300

    CustomerResource test

--- src/main/kotlin/com/example/demo/controller/CustomerResource.kt
+++ src/main/kotlin/com/example/demo/controller/CustomerResource.kt
@@ -23,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/customers")
 class CustomerResource(private val customerService: CustomerService) {
     @PostMapping
-    fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<String> {
-        val savedCustomer = this.customerService.save(customerDto.toEntity())
-        return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${savedCustomer.email} saved!")
+    fun saveCustomer(@RequestBody @Valid customerDto: CustomerDto): ResponseEntity<CustomerView> {
+        val savedCustomer:Customer = this.customerService.save(customerDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED).body(CustomerView(savedCustomer))
     }
 
     @GetMapping("/{id}")
